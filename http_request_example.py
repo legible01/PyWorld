4c70f6f29e9dd63bee44a8111612ab9ac38d07fb
KKS <legible01@gmail.com>
2017-01-13 19:58:52 +0900

    when it start. write URL as factor

--- http_request_example.py
+++ http_request_example.py
@@ -1,21 +1,6 @@
 import socket
+import sys
 
-
-
-print('다음과 같이 URL을 입력하세요. ex:www.naver.com')
-URL = input("input URL:")
-#잘못된 URL혹은 인터넷 불량시 오류메세지출력.
-try:
-    HOST = socket.gethostbyname(URL)
-except socket.gaierror as error:
-    print('주소를 가져올수가 없습니다.\n인터넷 연결 혹은 올바른 주소를 입력하세요\n',error)
-
-PORT = socket.getservbyname('http','tcp')
-
-#소켓 생성
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-#소켓 연결
-s.connect((HOST,PORT))
 def RECV(rType,HOST):
 
     message = rType+' / HTTP/1.1\r\nHost:'+URL+'\r\n\r\n\r\n'
@@ -35,6 +20,34 @@ def RECV(rType,HOST):
     print(rType + '내용 수신\n'+text)
     
 
+
+
+
+
+
+
+URL = str(sys.argv[1])#
+if ((len(sys.argv) >= 3) or (len(sys.argv) <= 1)):#인자값이 이상하게 받을경우 오류메세지 출력
+    print("실행파일 URL (ex:http_request_example www.naver.com) 의 형태로 다시 입력하세요.")
+    quit()
+    
+    
+    
+
+try:#잘못된 URL혹은 인터넷 불량시 오류메세지출력.
+    HOST = socket.gethostbyname(URL)
+except socket.gaierror as error:
+    print('주소를 가져올수가 없습니다.\n인터넷 연결 혹은 올바른 주소를 입력하세요\n',error)
+    quit()
+PORT = socket.getservbyname('http','tcp')
+
+#소켓 생성
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+#소켓 연결
+s.connect((HOST,PORT))
+
 RECV('GET',HOST)
 s.close()
+
 print('종료합니다.')
+
